fcdfced6e751c0da0bf901865b5f1f01b7893871
Serhan KORAL <serhan_koral@hotmail.com>
2023-02-10 15:24:18 +0300

    Proje dosyası ekle.

---
+++ Class Ayırma/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Class_Ayırma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisiler kisiler = new Kisiler();
+            kisiler.isim = "Serhan";
+            kisiler.soyisim = "Koral";
+            kisiler.kimlikNumarasi = 12345678901;
+            kisiler.Yazdir();
+
+        }
+    }
+    partial class Kisiler
+    {
+        //Burada tanımalamalar
+        public Int64 kimlikNumarasi { get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        //Fonksiyon tanımlarken private olur bunuda aynı prob da olduğu gibi ayni isim gibi gozuken fonksiyonlarla halledilir
+        partial void yazdir();
+        public void Yazdir() { yazdir(); }
+            
+
+    }
+    partial class Kisiler
+    {
+        //Burda Fonksiyonlar olur
+        partial void yazdir()
+        {
+            Console.WriteLine("Kimlik No={0}",kimlikNumarasi);
+            Console.WriteLine("İsim No={0}",isim);
+            Console.WriteLine("Soyisim No={0}",soyisim);
+        }
+
+    }
+}
